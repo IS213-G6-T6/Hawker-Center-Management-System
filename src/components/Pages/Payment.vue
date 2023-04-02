58a743aa133b3bd25c688ac3e09be5c3a290a269
dxtannn <88917272+dxtannn@users.noreply.github.com>
2023-04-02 21:00:23 +0800

    Update payment

--- src/components/Pages/Payment.vue
+++ src/components/Pages/Payment.vue
@@ -155,6 +155,56 @@ export default defineComponent({
     },
     components: { NavBar, Swiper, SwiperSlide, ReviewCard, MenuModal },
 })
+
+src="https://unpkg.com/axios/dist/axios.min.js"
+src="https://js.stripe.com/v2/"
+
+const form = document.getElementById('product-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+//   const name = document.getElementById('name').value;
+//   const price = document.getElementById('price').value;
+//   const quantity = document.getElementById('quantity').value;
+
+//   const data = {
+//     name: name,
+//     price: price * 100,
+//     quantity: quantity,
+//     orderID: "2"
+//   };
+
+const data = {
+    "customerID": "10",
+    "customer_name": "q",
+    "phone_no": "9420000",
+    "total_price": "100.00",
+    "status": "testing",
+    "order_items": [
+    {
+        "item_name": "rice",
+        "itemID": 9,
+        "quantity": 100
+    },
+    {
+        "item_name": "fish",
+        "itemID": 10,
+        "quantity": 200
+    }
+    ]
+};
+
+axios.post('http://localhost:5100/placeOrder', data)
+    .then(response => {
+    console.log(response.data)
+    window.open(response.data.url)
+    })
+    .catch(error => {
+    //console.error(error);
+    });
+});
+
 </script>
 
 <template>
@@ -191,35 +241,9 @@ export default defineComponent({
                 <div class="w-full md:relative md:block md:w-1/4 md:ml-4">
 
                     <div class="relative p-6 flex-auto">
-                        <div class="space-y-4 md:space-y-6">
-                            <div>
-                                <label
-                                    for="email"
-                                    class="block mb-2 text-sm font-medium text-gray-900"
-                                    >Your email</label
-                                >
-                                <input
-                                    id="email2"
-                                    v-model="email"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="name@company.com" />
-                            </div>
-                            <div>
-                                <label
-                                    for="password2"
-                                    class="block mb-2 text-sm font-medium text-gray-900"
-                                    >Password</label
-                                >
-                                <input
-                                    type="password"
-                                    name="password"
-                                    v-model="password"
-                                    id="password"
-                                    placeholder="••••••••"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                            </div>
+                        <div class="space-y-4 md:space-y-6" id="product-form">
                             <button
-                                @click="loginUser()"
+                                type = "submit"
                                 class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-5 rounded w-full">
                                 Checkout - ${{ this.totalAmount }}
                             </button>
@@ -249,5 +273,4 @@ export default defineComponent({
     font-weight: 700;
 }
 </style>
-
 
